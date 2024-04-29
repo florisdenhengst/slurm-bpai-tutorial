0995e36442babc3a1d5f3a576022ed2834341510
DJames03 <Feteridgedylan@gmail.com>
2024-04-29 15:17:46 +0200

    anaconda testg

---
+++ anaconda/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=pythonscript
+#SBATCH --time=1
+#SBATCH -N 1
+
+module load gnu9/9.4.0
+
+conda init
+conda activate anaconda-test
+cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340/
+cd $TMPDIR/dfe340/anaconda
+python script.py
+which python
