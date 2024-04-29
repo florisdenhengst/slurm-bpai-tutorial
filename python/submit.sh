900bc18872a61c6b4cef09f8b58e6bd8628de79b
DJames03 <Feteridgedylan@gmail.com>
2024-04-29 11:57:59 +0200

    fixed tmpdir

--- python/submit.sh
+++ python/submit.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #SBATCH --time=00:15:00
 #SBATCH -N 1
-cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR
-cd $TMPDIR/python
-python script.py
 
+mkdir -p $TMPDIR/dfe340
+cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR/dfe340
+cd $TMPDIR/dfe340/python
+python script.py
+rm -rf $TMPDIR/dfe340/python
