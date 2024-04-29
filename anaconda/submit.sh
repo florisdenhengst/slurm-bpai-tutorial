284cd4dd02c8e776fddf2dc5570b72d28578928e
DJames03 <Feteridgedylan@gmail.com>
2024-04-29 20:44:09 +0200

    conda name

--- anaconda/submit.sh
+++ anaconda/submit.sh
@@ -6,7 +6,7 @@
 module load gnu9/9.4.0
 
 conda init
-conda activate anaconda-test
+conda activate bpai
 cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340/
 cd $TMPDIR/dfe340/anaconda
 python script.py
