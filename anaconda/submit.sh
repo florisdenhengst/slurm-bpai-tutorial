cd73752e3486621ef66ee0adfd23ce715375c75a
DJames03 <Feteridgedylan@gmail.com>
2024-04-30 15:47:28 +0200

    add from old submit

--- anaconda/submit.sh
+++ anaconda/submit.sh
@@ -7,7 +7,9 @@ module load gnu9/9.4.0
 
 conda init
 conda activate bpai
+mkdir -p $TMPDIR/dfe340
 cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340/
 cd $TMPDIR/dfe340/anaconda
 python script.py
 which python
+rm -rf $TMPDIR/dfe340/python
