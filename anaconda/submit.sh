25ceaca93de77d64527c5907681ce7015fe1df71
DJames03 <Feteridgedylan@gmail.com>
2024-04-30 14:07:57 +0200

    fix

--- anaconda/submit.sh
+++ anaconda/submit.sh
@@ -6,7 +6,7 @@
 module load gnu9/9.4.0
 
 conda init
-conda activate bpai
+conda activate var/scratch/dfe340/anaconda3/envs/bpai/bin/python/bpai
 cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340/
 cd $TMPDIR/dfe340/anaconda
 python script.py
