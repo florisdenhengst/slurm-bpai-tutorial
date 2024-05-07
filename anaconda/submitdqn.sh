f819dbf9dd870b9669e981291deb2505d5e91047
DJames03 <Feteridgedylan@gmail.com>
2024-05-07 15:01:29 +0200

    own dqn, to use seed

---
+++ anaconda/submitdqn.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --time=00:15:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+
+mkdir -p $TMPDIR/dfe340
+cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340
+cd $TMPDIR/dfe340/anaconda/
+conda init
+conda activate bpai
+python dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id CartPole-v1 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
+mkdir -p $HOME/slurm-bpai-tutorial/results
+cp -r $TMPDIR/dfe340/python/wandb $HOME/slurm-bpai-tutorial/results
+rm -rf $TMPDIR/dfe340/python
