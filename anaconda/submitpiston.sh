6e16cb8aa01361443ceb4de2b5f417017bdd7c34
DJames03 <Feteridgedylan@gmail.com>
2024-06-01 14:42:38 +0200

    test cleanrl piston ppo

---
+++ anaconda/submitpiston.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --time=00:15:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+
+mkdir -p $TMPDIR/dfe340
+cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340
+cd $TMPDIR/dfe340/anaconda/
+conda init
+conda activate bpai
+python cleanPiston.py  --env-id pistonball_v6 --total-timesteps 500000 
+
+#python dqnslurm.py --seed $SLURM_ARRAY_TASK_ID --env-id pistonball_v6 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
+mkdir -p $HOME/slurm-bpai-tutorial/results
+cp -r $TMPDIR/dfe340/python/wandb $HOME/slurm-bpai-tutorial/results
+rm -rf $TMPDIR/dfe340/python
