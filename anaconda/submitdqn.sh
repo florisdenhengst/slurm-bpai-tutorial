57dc5745e9f02de7246f82741a4247f822184c5c
DJames03 <Feteridgedylan@gmail.com>
2024-06-01 14:08:28 +0200

    remove seed for now

--- anaconda/submitdqn.sh
+++ anaconda/submitdqn.sh
@@ -8,7 +8,9 @@ cp -r $HOME/slurm-bpai-tutorial/anaconda $TMPDIR/dfe340
 cd $TMPDIR/dfe340/anaconda/
 conda init
 conda activate bpai
-python dqnslurm.py --seed $SLURM_ARRAY_TASK_ID --env-id CartPole-v1 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
+python dqnslurm.py  --env-id CartPole-v1 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
+
+#python dqnslurm.py --seed $SLURM_ARRAY_TASK_ID --env-id CartPole-v1 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
 mkdir -p $HOME/slurm-bpai-tutorial/results
 cp -r $TMPDIR/dfe340/python/wandb $HOME/slurm-bpai-tutorial/results
 rm -rf $TMPDIR/dfe340/python
