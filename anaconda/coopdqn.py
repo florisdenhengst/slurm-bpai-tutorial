57fa58c321a40a7e9ac1db4837dab8d846e47c55
DJames03 <Feteridgedylan@gmail.com>
2024-06-12 21:52:17 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -67,6 +67,8 @@ class QNetwork(nn.Module):
             nn.Conv2d(64, 64, 3, stride=1),
             nn.ReLU(),
             nn.Flatten(),
+            print('here')
+            #if i change this so the dimensions are the same, it does not run at all(no print statemetns so it does not start the looop)
             #nn.Linear(1024, 512),
             nn.Linear(64 * 7 * 7, 512),
             nn.ReLU(),
