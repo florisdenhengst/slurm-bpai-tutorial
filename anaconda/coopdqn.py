f623c8e5eca91830b6c75cf2b747b44b044855e3
DJames03 <Feteridgedylan@gmail.com>
2024-06-12 20:17:46 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
                     #.permute((2,0,1)) prob wrong
                     #print((torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device)))
                     #q_values = q_network(torch.Tensor(obs[agent]).permute((2,0,1)).to(device))
-                    q_values = q_network(torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device))
+                    q_values = q_network(torch.Tensor(obs).permute((2,0,1)).unsqueeze(0).to(device))
                     actions[agent] = torch.argmax(q_values, dim=1).cpu().numpy()[0]
             elif agent == 'second_0':
                 #print(agent)
@@ -177,7 +177,7 @@ if __name__ == "__main__":
                     #.permute((2,0,1)) prob wrong
                     #print((torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device)))
                     #q_values2 = q_network2(torch.Tensor(obs[agent]).permute((2,0,1)).to(device))
-                    q_values2 = q_network2(torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device))
+                    q_values2 = q_network2(torch.Tensor(obs).permute((2,0,1)).unsqueeze(0).to(device))
                     actions[agent] = torch.argmax(q_values2, dim=1).cpu().numpy()[0]
         
         next_obs, rewards, terminations, truncations, infos = env.step(actions)
