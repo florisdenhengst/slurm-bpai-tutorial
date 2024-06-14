6e42ee394cb2945f273c510fc4d4a84c37e55c82
DJames03 <Feteridgedylan@gmail.com>
2024-06-14 10:13:49 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -225,7 +225,7 @@ if __name__ == "__main__":
                 loss2 = F.mse_loss(td_target2, old_val2)
 
                 if global_step % 100 == 0:
-                    writer.add_histogram("q_values", q_values, global_step)
+                    #writer.add_histogram("q_values", q_values, global_step)
                     print(f"Q-values at step {global_step}: {q_values.cpu().detach().numpy()}")
 
                     writer.add_scalar("losses/td_loss", loss, global_step)
