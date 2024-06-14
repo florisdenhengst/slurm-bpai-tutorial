e79483645e6f1cdd825732d6b352d3fd8a1fa350
DJames03 <Feteridgedylan@gmail.com>
2024-06-14 10:31:02 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
                 if random.random() < epsilon:
                     actions[agent] = env.action_space(agent).sample()
                 else:
-                    print('same problem?')
+                    
                     q_values = q_network(torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device))
                     actions[agent] = torch.argmax(q_values, dim=1).cpu().numpy()[0]
             elif agent == 'second_0':
@@ -178,8 +178,8 @@ if __name__ == "__main__":
                 if random.random() < epsilon:
                     actions[agent] = env.action_space(agent).sample()
                 else:
-                    print('im not at the error')
-                    print((torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device)).shape)
+                   
+                    #print((torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device)).shape)
                     q_values2 = q_network2(torch.Tensor(obs[agent]).permute((2,0,1)).unsqueeze(0).to(device))
                     actions[agent] = torch.argmax(q_values2, dim=1).cpu().numpy()[0]
         
