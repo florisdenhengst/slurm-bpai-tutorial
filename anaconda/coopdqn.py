1bf274121e070c809df2be4bc13cd900c82d44e8
DJames03 <Feteridgedylan@gmail.com>
2024-06-17 12:51:05 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
                 if global_step % 100 == 0:
                     #writer.add_histogram("q_values", q_values, global_step)
                     print(f"Q-values at step {global_step}: {q_values.cpu().detach().numpy()}")
-                    writer.add_scalars("Q-values", q_values_dict, global_step)
+                    #writer.add_scalars("Q-values", q_values_dict, global_step)
                     writer.add_scalar("losses/td_loss", loss, global_step)
                     writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
                     print("SPS:", int(global_step / (time.time() - start_time)))
