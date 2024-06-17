be0de5a809d2574bb5c54ff0df4a7761fd2a8737
DJames03 <Feteridgedylan@gmail.com>
2024-06-17 13:59:46 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
                     if truncations[agent]:
                         real_next_obs = infos[agent]["final_observation"]
                     reward_lst['first_0']+= rewards[agent]
-                    if q_values in locals():
+                    if 'q_values' in locals():
                         q_values_dict[f"{global_step}_{agent}"] = q_values.cpu().detach().numpy().tolist()
                     rb.add(obs[agent], real_next_obs, actions[agent], rewards[agent], terminations[agent], infos[agent])
 
