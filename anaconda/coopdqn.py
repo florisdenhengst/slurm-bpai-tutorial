c609684526b29b41ead633ae4097ebc266b65e7b
DJames03 <Feteridgedylan@gmail.com>
2024-06-17 18:59:53 +0200

    Update coopdqn.py

--- anaconda/coopdqn.py
+++ anaconda/coopdqn.py
@@ -247,6 +247,7 @@ if __name__ == "__main__":
                 if global_step % 100 == 0:
                     #writer.add_histogram("q_values", q_values, global_step)
                     print(f"Q-values at step {global_step}: {q_values.cpu().detach().numpy()}")
+                    print(f"Q-values at step {global_step}: {q_values}")
                     #writer.add_scalars("Q-values", q_values_dict, global_step)
                     writer.add_scalar("losses/td_loss", loss, global_step)
                     writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
