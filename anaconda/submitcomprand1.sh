3b99160b0dd65618c259381e5d72416a8da5fbe3
DJames03 <Feteridgedylan@gmail.com>
2024-06-24 15:42:02 +0200

    Update submitcomprand1.sh

--- anaconda/submitcomprand1.sh
+++ anaconda/submitcomprand1.sh
@@ -16,4 +16,3 @@ mkdir -p $HOME/slurm-bpai-tutorial/results
 cp -r $TMPDIR/dfe340/anaconda/wandb $HOME/slurm-bpai-tutorial/results
 cp -r $TMPDIR/dfe340/anaconda/runs $HOME/slurm-bpai-tutorial/results
 rm -rf $TMPDIR/dfe340/anaconda
-#test
