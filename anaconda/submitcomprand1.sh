7ab3432f35890d4b6fff7a23bd2ee412a8b2adc2
DJames03 <Feteridgedylan@gmail.com>
2024-06-24 15:48:19 +0200

    Update submitcomprand1.sh

--- anaconda/submitcomprand1.sh
+++ anaconda/submitcomprand1.sh
@@ -12,7 +12,7 @@ conda activate bpai
 python comprand.py --learning_rate 2.5e-3 --env-id entombed_competitive_v3 --total-timesteps 20000 --track --wandb-project-name cleanrldas --capture_video
 
 #python dqnslurm.py --seed $SLURM_ARRAY_TASK_ID --env-id entombed_competitive_v3 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
-mkdir -p $HOME/slurm-bpai-tutorial/results
-cp -r $TMPDIR/dfe340/anaconda/wandb $HOME/slurm-bpai-tutorial/results
-cp -r $TMPDIR/dfe340/anaconda/runs $HOME/slurm-bpai-tutorial/results
+mkdir -p $HOME/slurm-bpai-tutorial/anaconda/results
+cp -r $TMPDIR/dfe340/anaconda/wandb $HOME/slurm-bpai-tutorial/anaconda/results
+cp -r $TMPDIR/dfe340/anaconda/runs $HOME/slurm-bpai-tutorial/anaconda/results
 rm -rf $TMPDIR/dfe340/anaconda
